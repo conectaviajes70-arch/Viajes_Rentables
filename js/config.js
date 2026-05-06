c9a5b86f3bfd672ad8368d27a65bad842888fa43
conectaviajes70-arch <conectaviajes70@gmail.com>
2026-05-06 03:13:42 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js
+const CONFIG = {
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/TU_URL_AQUI/exec", // ← Cambia esto
+    GOOGLE_MAPS_KEY: "TU_CLAVE_GOOGLE_MAPS_AQUI" // ← Necesitas obtener una
+};
